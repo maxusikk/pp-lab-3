476c82c34ddf1c0da3102e419880eb0e9dae0244
Damian Ziółkowski <xdamianeeg@gmail.com>
2024-04-07 11:29:39 +0200

    final

--- main.java
+++ main.java
@@ -5,10 +5,8 @@ public class main {
     public static void main(String[] args) {
         Point center = new Point(0, 0);
 
-        // Create a ColoredCircle object with the center point, radius, and color
         ColoredCircle coloredCircle = new ColoredCircle(center, 5, "Red");
 
-        // Display the properties of the colored circle
         System.out.println("Colored Center: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
         System.out.println("Colored Radius: " + coloredCircle.getRadius());
         System.out.println("Colored Perimeter: " + coloredCircle.calculatePerimeter());
